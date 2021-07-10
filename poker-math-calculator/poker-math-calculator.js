6972d5f0d2d3f1e7e518d3e7ffd29ad7e231f915
allinall <allinallinall@protonmail.com>
2021-07-10 19:59:36 -0400

    Betsize Percentage Box functional

--- poker-math-calculator/poker-math-calculator.js
+++ poker-math-calculator/poker-math-calculator.js
@@ -1,3 +1,8 @@
+let $calcButton = document.querySelector("#calc-button");
+let $betSize = document.querySelector("#bet-size-input-box");
+let $potSize = document.querySelector("#pot-size-input-box");
+let $betSizePercentageBox = document.querySelector("#bet-size-percentage-box");
+let $betSlider = document.querySelector("#bet-slider");
 let $bet20 = document.querySelector("#b20");
 let $bet25 = document.querySelector("#b25");
 let $bet33 = document.querySelector("#b33");
@@ -5,29 +10,43 @@ let $bet50 = document.querySelector("#b50");
 let $bet67 = document.querySelector("#b67");
 let $bet75 = document.querySelector("#b75");
 let $bet100 = document.querySelector("#b100");
-let $betSlider = document.querySelector("#bet-slider");
 
-$bet20.addEventListener("click", function () {
-  $betSlider.value = 20;
-});
-$bet25.addEventListener("click", function () {
-  $betSlider.value = 25;
-});
-$bet33.addEventListener("click", function () {
-  $betSlider.value = 33.3;
-});
-$bet50.addEventListener("click", function () {
-  $betSlider.value = 50;
-});
-$bet67.addEventListener("click", function () {
-  $betSlider.value = 66.6;
-});
-$bet75.addEventListener("click", function () {
-  $betSlider.value = 75;
-});
-$bet100.addEventListener("click", function () {
-  $betSlider.value = 100;
+$calcButton.addEventListener("click", function () {
+  let betSize = Number($betSize.value);
+  let potSize = Number($potSize.value);
+  $betSlider.value = ((betSize/potSize)*100).toPrecision(4);
+  $betSizePercentageBox.innerHTML = $betSlider.value+"%";
 });
 
 $betSlider.addEventListener("change", function () {
+  $betSizePercentageBox.innerHTML = $betSlider.value+"%";
+});
+
+$bet20.addEventListener("click", function () {
+  $betSlider.value = 20;
+  $betSizePercentageBox.innerHTML = $betSlider.value+"%";
+});
+$bet25.addEventListener("click", function () {
+  $betSlider.value = 25;
+  $betSizePercentageBox.innerHTML = $betSlider.value+"%";
+});
+$bet33.addEventListener("click", function () {
+  $betSlider.value = 33.3;
+  $betSizePercentageBox.innerHTML = $betSlider.value+"%";
+});
+$bet50.addEventListener("click", function () {
+  $betSlider.value = 50;
+  $betSizePercentageBox.innerHTML = $betSlider.value+"%";
+});
+$bet67.addEventListener("click", function () {
+  $betSlider.value = 66.6;
+  $betSizePercentageBox.innerHTML = $betSlider.value+"%";
+});
+$bet75.addEventListener("click", function () {
+  $betSlider.value = 75;
+  $betSizePercentageBox.innerHTML = $betSlider.value+"%";
+});
+$bet100.addEventListener("click", function () {
+  $betSlider.value = 100;
+  $betSizePercentageBox.innerHTML = $betSlider.value+"%";
 });
